58e74e38b70fe030bb24566b0b47e1b88177a68e
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-25 15:11:38 +0800

    Add nlp model for emotion detection

---
+++ transformer.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+import json
+
+classifier = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base")
+# classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+# classifier = pipeline(task="text-classification", model="michellejieli/emotion_text_classifier")
+
+# Example with cleaned text
+text = "People keep looking at me different and I think that they're making fun of me."
+print(json.dumps(classifier(text), indent=2))

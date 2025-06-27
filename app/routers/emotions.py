146166856f409bbd0eac6c1c3dc8db5bb9e33262
Jose Neil Silagan <neilsilagan@gmail.com>
2025-06-27 18:41:00 +0800

    Create a sample post request

--- app/routers/emotions.py
+++ app/routers/emotions.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+
+class EmotionRequest(BaseModel):
+    text: str
 
 router = APIRouter(
     prefix="/emotions",
     tags=["emotions"],
 )
 
-@router.get("/")
-async def get_emotions():
-    return "sadboy"
+@router.post("/")
+async def get_emotions(request: EmotionRequest):
+    emotion_result = f"emotion mo ay tungkol sa : {request.text} ay: sadboy"
+    return emotion_result
